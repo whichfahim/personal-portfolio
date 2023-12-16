b361f2fb9095253da8d6b286cc2ecce1fad998ef
Fahim Imtiaz <fahim.imtiaz07@gmail.com>
2023-12-15 22:16:27 -0500

    Fahim | navbar | built and styled navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
+import Logo from '../assets/Logo.svg'
+import '../styles/Navbar.css'
+
 function Navbar(){
     return(
         <div className='navbar'>
-        <div className='logo'></div>
+        <div className='logo-container'><img className='logo' src={Logo} /></div>
         <div className='main-nav'>
           <ol className="main-nav-list">
-            <li><a className="main-nav-link" href="#">About</a> </li>
-            <li><a className="main-nav-link" href="#">Experience</a></li>
-            <li><a className="main-nav-link" href="#">Projects</a></li>
+            <li><a className="main-nav-link" href="#"><span className='u-txt-color-secondary'>01.</span> About</a> </li>
+            <li><a className="main-nav-link" href="#"><span className='u-txt-color-secondary'>02.</span> Experience</a></li>
+            <li><a className="main-nav-link" href="#"><span className='u-txt-color-secondary'>03.</span> Projects</a></li>
           </ol>
         </div>
       </div>
