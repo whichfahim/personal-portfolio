37f1cc56fbc28fee9802782a6124216424df6d12
Fahim Imtiaz <fahim.imtiaz07@gmail.com>
2024-05-13 21:40:24 -0400

    main | added projects to projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ function Projects() {
           <h4 className="project-heading u-text-color-primary">
             E-Learning Platform
           </h4>
-          <h4 className="project-subheading">Geniotech</h4>
+          <p className="project-subheading">Geniotech</p>
           <p>May 2023 - October 2024</p>
           <ul>
             <li>
@@ -33,15 +33,133 @@ function Projects() {
               nunc suspendisse tincidunt.
             </li>
           </ul>
-          <div className="project-doc--icons">
+          <div className="project-doc--icons-container">
             <a href="#" target="__blank">
-              <img src="src\assets\icons\Figma.png" alt="figma icon" />
+              <img
+                src="src\assets\icons\Figma.png"
+                className="project-doc--icon"
+                alt="figma icon"
+              />
             </a>
             <a href="#" target="__blank">
-              <img src="src\assets\icons\Behance.png" alt="behance logo" />
+              <img
+                src="src\assets\icons\Behance.png"
+                className="project-doc--icon"
+                alt="behance logo"
+              />
             </a>
             <a href="#" target="__blank">
-              <img src="src\assets\icons\Github.png" alt="github logo" />
+              <img
+                src="src\assets\icons\Github.png"
+                className="project-doc--icon"
+                alt="github logo"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="project-container">
+        <div className="project-img-container">
+          <img
+            src="src\assets\img\backtoplay.png"
+            alt="b2p cover image"
+            className="project-img"
+          />
+        </div>
+
+        <div className="project-description">
+          <h4 className="project-heading u-text-color-primary">Back To Play</h4>
+          <p className="project-subheading">BRAC IT</p>
+          <p>May 2023 - October 2024</p>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet consectetur. Curabitur lorem suscipit
+              purus hendrerit mi venenatis.
+            </li>
+            <li>
+              Tellus elementum elit ipsum sed nullam. Sit libero feugiat ipsum
+              ultrices lectus et sagittis arcu.
+            </li>
+            <li>
+              Erat purus condimentum mi nisl. Vel sit sit egestas nibh vitae
+              nunc suspendisse tincidunt.
+            </li>
+          </ul>
+          <div className="project-doc--icons-container">
+            <a href="#" target="__blank">
+              <img
+                src="src\assets\icons\Figma.png"
+                className="project-doc--icon"
+                alt="figma icon"
+              />
+            </a>
+            <a href="#" target="__blank">
+              <img
+                src="src\assets\icons\Behance.png"
+                className="project-doc--icon"
+                alt="behance logo"
+              />
+            </a>
+            <a href="#" target="__blank">
+              <img
+                src="src\assets\icons\Github.png"
+                className="project-doc--icon"
+                alt="github logo"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="project-container">
+        <div className="project-img-container">
+          <img
+            src="src\assets\img\glasscosmetics.png"
+            alt="glass cover image"
+            className="project-img"
+          />
+        </div>
+
+        <div className="project-description">
+          <h4 className="project-heading u-text-color-primary">
+            Glass Website Redesign
+          </h4>
+          <p className="project-subheading">Glass Cosmetics BD</p>
+          <p>May 2023 - October 2024</p>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet consectetur. Curabitur lorem suscipit
+              purus hendrerit mi venenatis.
+            </li>
+            <li>
+              Tellus elementum elit ipsum sed nullam. Sit libero feugiat ipsum
+              ultrices lectus et sagittis arcu.
+            </li>
+            <li>
+              Erat purus condimentum mi nisl. Vel sit sit egestas nibh vitae
+              nunc suspendisse tincidunt.
+            </li>
+          </ul>
+          <div className="project-doc--icons-container">
+            <a href="#" target="__blank">
+              <img
+                src="src\assets\icons\Figma.png"
+                className="project-doc--icon"
+                alt="figma icon"
+              />
+            </a>
+            <a href="#" target="__blank">
+              <img
+                src="src\assets\icons\Behance.png"
+                className="project-doc--icon"
+                alt="behance logo"
+              />
+            </a>
+            <a href="#" target="__blank">
+              <img
+                src="src\assets\icons\Github.png"
+                className="project-doc--icon"
+                alt="github logo"
+              />
             </a>
           </div>
         </div>
