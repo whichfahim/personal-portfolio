670f1d1bd2531a8566dde1efa9072aa68fe79983
Fahim Imtiaz <fahim.imtiaz07@gmail.com>
2024-05-16 21:40:33 -0400

    main | nav tabs working

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,194 @@
 import "../styles/Experience.scss";
 
 function Experience() {
-  return <section className="section-experience"></section>;
+  return (
+    <section className="section-experience">
+      <h1 className="u-heading-secondary">Work Experience</h1>
+
+      <div className="nav-tabs-container">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link active"
+              id="geniotech-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#geniotech"
+              type="button"
+              role="tab"
+              aria-controls="geniotech"
+              aria-selected="true"
+            >
+              Geniotech
+            </button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link"
+              id="BRAC-IT-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#BRAC-IT"
+              type="button"
+              role="tab"
+              aria-controls="BRAC-IT"
+              aria-selected="false"
+            >
+              BRAC IT
+            </button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link"
+              id="NAL-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#NAL"
+              type="button"
+              role="tab"
+              aria-controls="NAL"
+              aria-selected="false"
+            >
+              NAL Ltd.
+            </button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link"
+              id="freelance-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#freelance"
+              type="button"
+              role="tab"
+              aria-controls="freelance"
+              aria-selected="false"
+            >
+              Freelance
+            </button>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div
+            class="tab-pane fade show active"
+            id="geniotech"
+            role="tabpanel"
+            aria-labelledby="geniotech-tab"
+          >
+            <h2>Software Engineer Intern</h2>
+            <p>September 2023 - April 2024</p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur. Eu risus fringilla
+                maecenas eu nec ante sit. Eu vel feugiat tempus morbi risus
+                viverra bibendum ullamcorper quis. Urna vel urna semper blandit
+                amet ultrices viverra. Sed tincidunt sagittis ipsum fusce.
+                Congue consectetur mattis fusce id.
+              </li>
+              <li>
+                Imperdiet enim urna eget faucibus in ultricies massa. Lorem arcu
+                vel volutpat rhoncus leo morbi blandit. Cursus vitae lacus risus
+                blandit sagittis tempor. In adipiscing sed etiam morbi pretium.
+                Ut tincidunt amet nunc vitae cursus. Odio ullamcorper
+                sollicitudin curabitur sed est faucibus.
+              </li>
+              <li>
+                Non laoreet augue porttitor imperdiet vivamus. Viverra neque
+                gravida faucibus imperdiet a quam tincidunt. Potenti purus
+                pulvinar arcu nec turpis facilisis neque accumsan. Et.
+              </li>
+            </ul>
+          </div>
+          <div
+            class="tab-pane fade show active"
+            id="BRAC-IT"
+            role="tabpanel"
+            aria-labelledby="BRAC-IT-tab"
+          >
+            <h2>Software Engineer Intern</h2>
+            <p>September 2023 - April 2024</p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur. Eu risus fringilla
+                maecenas eu nec ante sit. Eu vel feugiat tempus morbi risus
+                viverra bibendum ullamcorper quis. Urna vel urna semper blandit
+                amet ultrices viverra. Sed tincidunt sagittis ipsum fusce.
+                Congue consectetur mattis fusce id.
+              </li>
+              <li>
+                Imperdiet enim urna eget faucibus in ultricies massa. Lorem arcu
+                vel volutpat rhoncus leo morbi blandit. Cursus vitae lacus risus
+                blandit sagittis tempor. In adipiscing sed etiam morbi pretium.
+                Ut tincidunt amet nunc vitae cursus. Odio ullamcorper
+                sollicitudin curabitur sed est faucibus.
+              </li>
+              <li>
+                Non laoreet augue porttitor imperdiet vivamus. Viverra neque
+                gravida faucibus imperdiet a quam tincidunt. Potenti purus
+                pulvinar arcu nec turpis facilisis neque accumsan. Et.
+              </li>
+            </ul>
+          </div>
+          <div
+            class="tab-pane fade show active"
+            id="NAL"
+            role="tabpanel"
+            aria-labelledby="NAL-tab"
+          >
+            <h2>Software Engineer Intern</h2>
+            <p>September 2023 - April 2024</p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur. Eu risus fringilla
+                maecenas eu nec ante sit. Eu vel feugiat tempus morbi risus
+                viverra bibendum ullamcorper quis. Urna vel urna semper blandit
+                amet ultrices viverra. Sed tincidunt sagittis ipsum fusce.
+                Congue consectetur mattis fusce id.
+              </li>
+              <li>
+                Imperdiet enim urna eget faucibus in ultricies massa. Lorem arcu
+                vel volutpat rhoncus leo morbi blandit. Cursus vitae lacus risus
+                blandit sagittis tempor. In adipiscing sed etiam morbi pretium.
+                Ut tincidunt amet nunc vitae cursus. Odio ullamcorper
+                sollicitudin curabitur sed est faucibus.
+              </li>
+              <li>
+                Non laoreet augue porttitor imperdiet vivamus. Viverra neque
+                gravida faucibus imperdiet a quam tincidunt. Potenti purus
+                pulvinar arcu nec turpis facilisis neque accumsan. Et.
+              </li>
+            </ul>
+          </div>
+          <div
+            class="tab-pane fade show active"
+            id="freelance"
+            role="tabpanel"
+            aria-labelledby="freelance-tab"
+          >
+            <h2>Software Engineer Intern</h2>
+            <p>September 2023 - April 2024</p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur. Eu risus fringilla
+                maecenas eu nec ante sit. Eu vel feugiat tempus morbi risus
+                viverra bibendum ullamcorper quis. Urna vel urna semper blandit
+                amet ultrices viverra. Sed tincidunt sagittis ipsum fusce.
+                Congue consectetur mattis fusce id.
+              </li>
+              <li>
+                Imperdiet enim urna eget faucibus in ultricies massa. Lorem arcu
+                vel volutpat rhoncus leo morbi blandit. Cursus vitae lacus risus
+                blandit sagittis tempor. In adipiscing sed etiam morbi pretium.
+                Ut tincidunt amet nunc vitae cursus. Odio ullamcorper
+                sollicitudin curabitur sed est faucibus.
+              </li>
+              <li>
+                Non laoreet augue porttitor imperdiet vivamus. Viverra neque
+                gravida faucibus imperdiet a quam tincidunt. Potenti purus
+                pulvinar arcu nec turpis facilisis neque accumsan. Et.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Experience;
