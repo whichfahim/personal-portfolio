5a9d1171b1371e353531554b0fa74d620e8f4821
Fahim Imtiaz <fahim.imtiaz07@gmail.com>
2024-05-24 22:06:25 -0400

    navbar | responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,39 @@
-import Logo from '../assets/Logo.svg'
-import '../styles/Navbar.css'
-
-function Navbar(){
-    return(
-        <div className='navbar'>
-        <div className='logo-container'><img className='logo' src={Logo} /></div>
-        <div className='main-nav'>
-          <ol className="main-nav-list">
-            <li><a className="main-nav-link" href="#"><span className='u-txt-color-secondary'>01.</span> About</a> </li>
-            <li><a className="main-nav-link" href="#"><span className='u-txt-color-secondary'>02.</span> Experience</a></li>
-            <li><a className="main-nav-link" href="#"><span className='u-txt-color-secondary'>03.</span> Projects</a></li>
-          </ol>
+import logo from "../assets/Logo.svg";
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src={logo} className="nav-logo" alt="logo" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="#">
+            Home <span class="sr-only">(current)</span>
+          </a>
+          <a class="nav-item nav-link" href="#">
+            Features
+          </a>
+          <a class="nav-item nav-link" href="#">
+            Pricing
+          </a>
+          <a class="nav-item nav-link disabled" href="#">
+            Disabled
+          </a>
         </div>
       </div>
-    )
+    </nav>
+  );
 }
 
 export default Navbar;
